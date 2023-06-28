04b8327a09f25d68c20342384c5639824d94d031
aciDrums7 <edoardo.carradori.96@gmail.com>
2023-06-28 23:06:18 +0200

    Lesson 7: More Coverage

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -12,6 +12,12 @@ contract FundMeTest is Test {
     uint256 constant SEND_VALUE = 0.1 ether; //4 100000000000000000
     uint256 constant STARTING_BALANCE = 10 ether;
 
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+
     function setUp() external {
         //1 us -> fundMeTest -> FundMe
         //2 us == msg.sender
@@ -21,10 +27,6 @@ contract FundMeTest is Test {
         fundMe = deployFundMe.run();
         vm.deal(USER, STARTING_BALANCE);
     }
-
-    /* function testMinimumDollarIsFive() public {
-        assertEq(fundMe.get(), 5e18);
-    } */
 
     function testOwnerIsMsgSender() public {
         /*  console.log("FundMe Owner: ");
@@ -42,10 +44,10 @@ contract FundMeTest is Test {
     //1     - Testing a specific part of our code
     //2 2. Integration
     //2     - Testing how our code works with other parts of our code
-    //5 3. Forked
-    //5     - Testing our code on a simulated real environment
-    //7 4. Staging
-    //7     - Testing our code in a real environment that is not prod
+    //3 3. Forked
+    //3     - Testing our code on a simulated real environment
+    //4 4. Staging
+    //4     - Testing our code in a real environment that is not prod
     function testPriceFeedVersionIsAccurate() public {
         assertEq(fundMe.getVersion(), 4);
     }
@@ -61,6 +63,64 @@ contract FundMeTest is Test {
         fundMe.fund{value: SEND_VALUE}();
         uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
         assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public funded {
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.expectRevert();
+        vm.prank(USER);
+        fundMe.withdraw();
+    }
+
+    function testWithdrawWithASingleFunder() public funded {
+        // 1. Arrange
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // 2. Act
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+
+        // 3. Assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(endingOwnerBalance, startingOwnerBalance + startingFundMeBalance);
+    }
+
+    function testWithdrawFromMultipleFunders() public {
+        // 1. Arrange
+        //1 uint160 needed to generate addresses from numbers
+        //1 https://stackoverflow.com/questions/43318077/solidity-type-address-not-convertible-to-type-uint256
+        uint160 numberOfFunders = 7;
+        //2 sometimes address(0) reverts in tests, so better start from address(1)
+        uint160 startingFunderIndex = 1;
+        for(uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            //1 vm.prank new address
+            //2 vm.deal new address
+            //3 hoax -> prank + deal
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value:SEND_VALUE}();
+            //4 fund the fundMe
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        // 2. Act
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        // 3. Assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(endingOwnerBalance, startingOwnerBalance + startingFundMeBalance);
     }
 }
 
